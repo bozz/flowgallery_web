4252f68a8c0c711d519ea98d853b8d040470f906
Boris Searles <boris@lucidgardens.com>
2012-07-24 11:48:08 +0200

    bugfix: change goto method to jump; remove debug outputs

--- js/script.js
+++ js/script.js
@@ -88,7 +88,6 @@ $(function() {
      * @param {string} key demo key, used as cache key or to build remote url
      */
     var demoSelectionHandler = function(key) {
-      console.log("aaaaaaaaaaaaaa");
       setGalleriesEnabled(false);
       if(sectionCache[key]) {
         $('#demo-sections > div.section').hide();
@@ -98,9 +97,8 @@ $(function() {
         setGalleriesEnabled(true);
 
         var flowgallery = sectionCache[key].find('div.demo ul').data('flowgallery');
-        flowgallery.goto(0, false);
+        flowgallery.jump(0, false);
 
-        console.log("flowgall: ", flowgallery, flowgallery.getOptions());
         // refocus selected demo
         $('#nav a[href=#demos]').click();
       } else {
